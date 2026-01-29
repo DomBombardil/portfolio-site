8b5e33a028d8eb1b409d9d338090404f8e359099
dominik <dominik@protonmail.com>
2026-01-29 09:15:31 +0100

    Added an atribute to Project model.

--- projects/models.py
+++ projects/models.py
@@ -6,6 +6,7 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
+    repository_link = models.CharField(max_length=200, null=True, blank=True)
     image = models.CharField(max_length=200)
 
     def __str__(self):
