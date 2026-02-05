d1da0a261f18251d27eca4635275e78cb6725b86
dominik <dominik@protonmail.com>
2026-02-05 16:03:11 +0100

    Added a new database default, and added more requirements for the live version

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -103,11 +103,14 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL", "sqlite:///db.sqlite3"),
+        conn_max_age=600,
+        ssl_require=True,  # Render Postgres typically expects SSL
+    )
 }
 
 
