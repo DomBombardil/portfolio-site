ec8eff3feaf3053fc9d4f3d419f2d81e6291a072
dominik <dominik@protonmail.com>
2026-02-07 12:02:25 +0100

    Added a local environment capability

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -19,20 +19,21 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-egke1!-4@@2(ur&ro7@11^h2$%(bhws@oyv7%k4!6u7f0gyx3z'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 import os
 
-DEBUG = os.getenv('DEBUG', '0') == '1'
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-only-secret-key")
+DEBUG = os.getenv("DEBUG", "1") == "1"  
 
 ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "[::1]",
     "dominik-ivacic.com",
     "www.dominik-ivacic.com",
     "dominik-ivacic-portfolio.onrender.com",
 ]
+
 
 # Render public hostname
 render_host = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
