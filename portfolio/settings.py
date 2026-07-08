6820701ca2beeae9afc0cde7ae2af3836cf0e701
dominik <dominik@protonmail.com>
2026-07-08 13:44:37 +0200

    Added an email timeout variable.

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -17,7 +17,8 @@ from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv(BASE_DIR / ".env.local")
+load_dotenv(BASE_DIR / ".env")
+EMAIL_TIMEOUT = int(os.getenv("EMAIL_TIMEOUT", "10"))
 
 
 # Quick-start development settings - unsuitable for production
